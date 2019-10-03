7efba508261056c27565ff2e23168914748a672c
“mgutierrezc” <a20141676@pucp.pe>
2019-10-02 22:52:39 -0500

    Cambios completos en el models, falta cambiar pages

--- FT2001/models.py
+++ FT2001/models.py
@@ -10,17 +10,46 @@ from otree.api import (
 )
 
 
-author = 'Your name here'
+author = 'Marco Gutierrez'
 
 doc = """
-Your app description
+Fehr and Tyran 2001 Nominal Treatment Design
 """
 
 
 class Constants(BaseConstants):
     name_in_url = 'FT2001'
-    players_per_group = None
-    num_rounds = 1
+    players_per_group = 4
+    num_rounds = 30
+    # Pre and Post Shock values of the money
+    money = [42, 14]
+    # Shock Round
+    shock_round = 15
+
+    # todo: Crear un config.py que tenga los valores de equilibrio para FT2001 y LP2014, y hacer que las constantes
+    #  cambien para todo el juego de acuerdo a eso (con un custom session config parameter)
+    # Valores de equilibrio en FT2001
+    eq_prices = dict(x=9, y=27)
+    # Los valores siguientes son los que ven x e y en los demas
+    eq_avg_prices_others = dict(x=21, y=15)
+
+    # Constantes de func de pagos FT2001
+    a = 0.5
+    b = 0.6
+    c = 27
+    d = 1
+    e = 0.05
+    f = 20
+    V = 40
+
+    confidence_choices = [
+                 [1, 'Desconfío totalmente que mis expectativas sean correctas'],
+                 [2, 'Desconfío mucho que mis expectativas sean correctas'],
+                 [3, 'Desconfío un poco que mis expectativas sean correctas'],
+                 [4, 'Confío un poco que mis expectativas sean correctas'],
+                 [5, 'Confío mucho que mis expectativas sean correctas'],
+                 [6, 'Confío totalmente que mis expectativas sean correctas']
+                 ]
 
 
 class Subsession(BaseSubsession):
@@ -28,8 +57,22 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+    sum_prices = models.FloatField(min=0, initial=0)
+    shock_period = models.IntegerField()
 
 
 class Player(BasePlayer):
-    pass
+    selling_price = models.FloatField(min=0, max=30, label='Tu precio de venta')
+    others_avg_price = models.FloatField(min=0, max=30, label='Precio promedio de los demás')
+    expected_price = models.FloatField(min=0, max=30, label='¿Cuál crees que sea el precio promedio de los demás?')
+    confidence = models.CharField(
+        choices=Constants.confidence_choices,
+        label='¿Qué tanto confías en que el precio promedio que esperas que tengan los demás sea correcto?',
+        widget=widgets.RadioSelect)
+
+    def role(self):
+        if (self.id_in_group == 1) or (self.id_in_group == 2):
+            return 'x'
+        elif (self.id_in_group == 3) or (self.id_in_group == 4):
+            return 'y'
+
--- FT2001/pages.py
+++ FT2001/pages.py
@@ -1,10 +1,39 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
+import math
 
 
-class MyPage(Page):
+class Introduction(Page):
     pass
+
+
+class SettingPrice(Page):
+    form_model = 'player'
+    form_fields = ['selling_price', 'expected_price', 'confidence']
+
+    def before_next_page(self):
+        con = Constants
+        players = self.group.get_players()
+        player = self.player
+        group = self.group
+        for p in players:
+            self.group.sum_prices += p.selling_price
+
+        # todo: Actualizar avg price al modelo de PW, porque ahora está en versión FT
+        if self.round_number <= con.shock_round:
+            group.shock_period = 0
+        elif self.round_number > con.shock_round:
+            group.shock_period = 1
+
+        player.others_avg_price = (group.sum_prices - player.selling_price)/\
+                                       (con.players_per_group-1)
+
+        # Definiendo func de pagos reales FT2001
+        delta = (player.others_avg_price/con.money[group.shock_period] - con.eq_avg_prices_others[player.role()]/con.money[group.shock_period])
+        epsilon = player.selling_price/con.money[group.shock_period] - con.eq_prices[player.role()]/con.money[group.shock_period]
+        player.payoff = con.V*((1+con.a*delta^2)/(1+con.b*delta^2))/\
+                        (1+con.c*(epsilon-con.d*delta+con.e*math.atan(con.f*delta)))
 
 
 class ResultsWaitPage(WaitPage):
@@ -13,7 +42,21 @@ class ResultsWaitPage(WaitPage):
 
 
 class Results(Page):
-    pass
+    def vars_for_template(self):
+        # Aux variables
+        player = self.player
+        players = self.group.get_players()
+
+        # Vars for the template
+        price = player.selling_price
+        actual_avg_price_others = player.others_avg_price
+        income = self.player.payoff
+        total_income = 0 # Initializing the total_income value
+
+        for p in players:
+            total_income += self.p.payoff
+
+        return dict(price=price, actual_avg=actual_avg_price_others, income=income)
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [Introduction, ResultsWaitPage, Results]
